06f68d4ba3b185add50bca9b9b9b6e2820b03530
DlnKot <prod408@yandex.ru>
2026-03-13 17:26:02 +0400

    update number version

--- src/renderer-vue/src/App.vue
+++ src/renderer-vue/src/App.vue
@@ -111,7 +111,7 @@ import SettingsView from './components/SettingsView.vue'
 import ConnectionModal from './components/ConnectionModal.vue'
 import FirstRunModal from './components/FirstRunModal.vue'
 
-const appVersion = '0.2.0'
+const appVersion = '0.2.1'
 
 const { 
   connections, 
